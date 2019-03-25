8d4ba95ee669c4be29a435b1b814276ca6e7e1ce
Cody Laptop <codyswick1988@gmail.com>
2019-03-24 20:13:47 -0600

    skills

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ function toggleMenu() {
 
 
 
-
+// contact form
 $(".form").on('click', function() {
   $(this).addClass('active');
 });
@@ -54,4 +54,26 @@ $(".submit").on('click', function() {
 $(".ok_message").on('click', function() {
   $(this).removeClass("active");
   $(".form").removeClass("active").show();
+});
+
+
+//skills bars
+
+jQuery(document).ready(function(){
+
+  jQuery('.progress-bar').each(function() {
+    jQuery(this).find('.progress-content').animate({
+      width:jQuery(this).attr('data-percentage')
+    },2000);
+
+    jQuery(this).find('.progress-number-mark').animate(
+      {left:jQuery(this).attr('data-percentage')},
+      {
+       duration: 2000,
+       step: function(now, fx) {
+         var data = Math.round(now);
+         jQuery(this).find('.percent').html(data + '%');
+       }
+    });
+  });
 });
